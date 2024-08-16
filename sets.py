9b7355a383c987104143009b25758d05ba4f106f
Rogerio Panizza <67240761+RogerioPanizza@users.noreply.github.com>
2024-08-16 00:20:56 -0300

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,29 @@
+# Microatividade 3
+
+set_inicial = {11, 12, 13, 14}
+
+print(set_inicial)
+
+set_inicial.add(15)
+
+print(set_inicial)
+
+set_inicial.update([1, 2, 3, 4, 5])
+
+print(set_inicial)
+
+set_inicial.discard(13)
+
+print(set_inicial)
+
+novo_set = {20, 21, 23, 1, 2}
+
+print(novo_set)
+
+print(set_inicial | novo_set)
+
+print(set_inicial & novo_set)
+
+print(set_inicial - novo_set)
+
+print(set_inicial ^ novo_set)
---
+++ tuplas.py
@@ -0,0 +1,11 @@
+# Microatividade 02
+
+tupla = (1, 2, 3, 4, "Olá, tupla")
+
+print(tupla)
+
+print(tupla[4])
+
+print(tupla.index(3))
+
+print(tupla.index(33))
